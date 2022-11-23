5dc828096aa3495c1bfa08bbf5729289c4256f3d
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2022-11-23 14:49:40 +0200

    Some changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import axios from "axios";
+
+const API_KEY  = '31511712-b53d42f48d96ff6235f6befd4';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+async function getImages() {
+
+    const params =  {
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+
+    }
+
+    const res = await axios.get(`?key=${API_KEY}&q=cat&`, params);
+    const i = res.data;
+    const f = i.hits
+    console.log({});
+}
+
+getImages();
+
+
+
+
+
