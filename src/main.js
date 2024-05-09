f01998e29e672af2821ca8ae899d6be0a59b21e8
Kostiantyn Honcharov <goncharov.kostiantyn@gmail.com>
2024-05-09 21:45:57 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -25,7 +25,6 @@ searchForm.addEventListener('submit', (e) => {
 
 })
 
-
 async function fetchArticles(value) {
   try {
       const res = await axios.get(`?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true`);
@@ -46,53 +45,3 @@ async function fetchArticles(value) {
 }
 
 
-
-
-// const searchForm = document.querySelector('.js-search-form');
-// // const formInput = document.querySelector('.form-control')
-
-// searchForm.addEventListener('submit', onSearch);
-
-// async function onSearch (e) {
-//   e.preventDefault();
-
-//   form = (e.currentTarget.elements.searchQuery.value).trim();
-//   // localStorage.setItem('value', form);
-
-//   if (form === null || form === '') {
-//     iziToast.info({
-//     title: 'Please type something in the search input',
-//     // message: 'Welcome!',
-//     });
-//   }
-
-//   console.log(form);
-//   // clearArticlesContainer();
-//   // resetPage();
-//   fetchArticles(form);
-
-//   searchBtn.disabled = true;
-// }
-
-// async function fetchArticles(form) {
-//   try {
-//     const res = await axios.get(`?key=${API_KEY}&q=${form}&image_type=photo&orientation=horizontal&safesearch=true`);
-//     console.log(res.data.hits);
-//     const photos = res.data.hits;
-
-       
-//     if (photos.length === 0) {
-        
-//       iziToast.error({
-//         title: 'Sorry, there are no images matching your search query. Please try again.',
-//         message: 'Illegal operation',
-//       });
-//     }
-
-//                 // clearArticlesContainer();
-//         return;
-//   }
-//   catch(error) {
-//     console.log(error);
-// }
-// }
