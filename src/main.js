e3426dbc83781b3c83d8214910714b5ccb1861aa
Kostiantyn Honcharov <goncharov.kostiantyn@gmail.com>
2024-05-09 19:15:08 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,26 +6,94 @@ const API_KEY  = '31511712-b53d42f48d96ff6235f6befd4';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 
-const searchForm = document.querySelector('.js-search-form');
-const formInput = document.querySelector('.form-control')
+const searchForm = document.querySelector('.search-form');
 
-searchForm.addEventListener('submit', onSearch);
-
-async function onSearch (e) {
+searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  form = (e.currentTarget.elements.searchQuery.value).trim();
-  // localStorage.setItem('value', form);
-
-  if (form === null || form === '') {
-    Notify.info('Please type something in the search input.');
-    return;
+  let form = (e.currentTarget.elements.searchQuery.value).trim();
+   if (form === null || form === '') {
+    iziToast.info({
+    title: 'Please type something in the search input',
+    // message: 'Welcome!',
+    });
+   } else {
+     fetchArticles(form)
+     console.log(form);
   }
 
-  console.log(form);
-  clearArticlesContainer();
-  resetPage();
-  fetchArticles(form, currentPage);
+})
 
-  searchBtn.disabled = true;
+
+async function fetchArticles(value) {
+  try {
+      const res = await axios.get(`?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true`);
+      console.log(res.data.hits);
+    const photos = res.data.hits;
+    
+        if (photos.length === 0) {
+        
+      iziToast.error({
+        title: 'Sorry, there are no images matching your search query. Please try again.',
+        message: 'Illegal operation',
+      });
+        }
+    
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+
+
+fetchArticles();
+
+
+// const searchForm = document.querySelector('.js-search-form');
+// // const formInput = document.querySelector('.form-control')
+
+// searchForm.addEventListener('submit', onSearch);
+
+// async function onSearch (e) {
+//   e.preventDefault();
+
+//   form = (e.currentTarget.elements.searchQuery.value).trim();
+//   // localStorage.setItem('value', form);
+
+//   if (form === null || form === '') {
+//     iziToast.info({
+//     title: 'Please type something in the search input',
+//     // message: 'Welcome!',
+//     });
+//   }
+
+//   console.log(form);
+//   // clearArticlesContainer();
+//   // resetPage();
+//   fetchArticles(form);
+
+//   searchBtn.disabled = true;
+// }
+
+// async function fetchArticles(form) {
+//   try {
+//     const res = await axios.get(`?key=${API_KEY}&q=${form}&image_type=photo&orientation=horizontal&safesearch=true`);
+//     console.log(res.data.hits);
+//     const photos = res.data.hits;
+
+       
+//     if (photos.length === 0) {
+        
+//       iziToast.error({
+//         title: 'Sorry, there are no images matching your search query. Please try again.',
+//         message: 'Illegal operation',
+//       });
+//     }
+
+//                 // clearArticlesContainer();
+//         return;
+//   }
+//   catch(error) {
+//     console.log(error);
+// }
+// }
