93bc29ac115096f2175150d620cddf9739d1f19a
Kostiantyn Honcharov <goncharov.kostiantyn@gmail.com>
2024-05-09 23:30:00 +0300

    update styles

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+const articleContainer = document.querySelector('.gallery');
+
 const API_KEY  = '31511712-b53d42f48d96ff6235f6befd4';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
@@ -26,22 +28,54 @@ searchForm.addEventListener('submit', (e) => {
 })
 
 async function fetchArticles(value) {
-  try {
-      const res = await axios.get(`?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true`);
-      console.log(res.data.hits);
+ 
+
+    const res = await axios.get(`?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true`);
+    console.log(res.data.hits);
     const photos = res.data.hits;
     
-        if (photos.length === 0) {
+    if (photos.length === 0) {
         
       iziToast.error({
         title: 'Sorry, there are no images matching your search query',
         position: 'topRight',
-      });
-        }
-    
-  } catch (error) {
-    console.log(error);
+      })
+    } else {
+      renderGallery(photos);
+    }
   }
+
+
+function renderGallery(images) {
+  const createMarkupPage = images.map(article => {
+    return `
+    <div class="photo-card">
+        <a class ="thumb" href="${article.largeImageURL}"><img class="img" src="${article.webformatURL}" alt="${article.tags}" loading="lazy" /> </a> 
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+          ${article.likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+          ${article.views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+          ${article.comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+          ${article.downloads}
+        </p>
+      </div>
+    </div>
+  `;
+})
+.join('');
+
+   articleContainer.insertAdjacentHTML('beforeend', createMarkupPage);
+
 }
 
 
