5e22c963386da4a06e76912c92d1df0b5e2a4fca
KostiantynFirst <68106610+KostiantynFirst@users.noreply.github.com>
2024-05-11 20:41:24 +0000

    Deploying to gh-pages from @ KostiantynFirst/goit-js-hw-11@cda0b76eab143da518bf0ac4cd6593d1b3acbcdb 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,24 +1,24 @@
-import{a,i as c,S as u}from"./assets/vendor-09d7c26e.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))o(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function i(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(t){if(t.ep)return;t.ep=!0;const r=i(t);fetch(t.href,r)}})();const m=(s,e)=>{const i=e.map(o=>`
+import{a as c,i as l,S as d}from"./assets/vendor-6e0bf343.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))r(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const n of o.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&r(n)}).observe(document,{childList:!0,subtree:!0});function i(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function r(t){if(t.ep)return;t.ep=!0;const o=i(t);fetch(t.href,o)}})();const u=(s,e)=>{const i=e.map(r=>`
     <div class="photo-card">
-        <a class ="thumb" href="${o.largeImageURL}"><img class="img" src="${o.webformatURL}" alt="${o.tags}" loading="lazy" /> </a> 
+        <a class ="thumb" href="${r.largeImageURL}"><img class="img" src="${r.webformatURL}" alt="${r.tags}" loading="lazy" /> </a> 
       <div class="info">
         <p class="info-item">
           <b>Likes</b>
-          ${o.likes}
+          ${r.likes}
         </p>
         <p class="info-item">
           <b>Views</b>
-          ${o.views}
+          ${r.views}
         </p>
         <p class="info-item">
           <b>Comments</b>
-          ${o.comments}
+          ${r.comments}
         </p>
         <p class="info-item">
           <b>Downloads</b>
-          ${o.downloads}
+          ${r.downloads}
         </p>
       </div>
     </div>
-  `).join("");s.insertAdjacentHTML("beforeend",i)},d="31511712-b53d42f48d96ff6235f6befd4";a.defaults.baseURL="https://pixabay.com/api/";async function p(s){try{return(await a.get(`?key=${d}&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`)).data.hits}catch(e){throw e}}const l=document.querySelector(".gallery"),f=document.querySelector(".search-form");f.addEventListener("submit",h);function h(s){s.preventDefault();let e=s.currentTarget.elements.searchQuery.value.trim();e===null||e===""?c.info({title:"Please type something in the search input",position:"topRight"}):(g(e),console.log(e),f.reset(),y())}async function g(s){try{const e=await p(s);e.length===0?c.error({title:"Sorry, there are no images matching your search query",position:"topRight"}):m(l,e),new u(".gallery a").refresh()}catch(e){console.log(e)}}function y(){l.innerHTML=""}
+  `).join("");s.insertAdjacentHTML("beforeend",i)},m="31511712-b53d42f48d96ff6235f6befd4";c.defaults.baseURL="https://pixabay.com/api/";async function p(s){try{return(await c.get(`?key=${m}&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`)).data.hits}catch(e){throw e}}const a=document.querySelector(".gallery"),h='<div id="loader" class="loader"></div>',f=document.querySelector(".search-form");f.addEventListener("submit",g);function g(s){s.preventDefault();let e=s.currentTarget.elements.searchQuery.value.trim();e===null||e===""?l.info({title:"Please type something in the search input",position:"topRight"}):(y(e),console.log(e),f.reset(),b())}async function y(s){try{a.insertAdjacentHTML("beforebegin",h);const e=await p(s),i=document.querySelector("#loader");i&&i.remove(),e.length===0?l.error({title:"Sorry, there are no images matching your search query",position:"topRight"}):u(a,e),new d(".gallery a").refresh()}catch(e){console.log(e)}}function b(){a.innerHTML=""}
 //# sourceMappingURL=commonHelpers.js.map
